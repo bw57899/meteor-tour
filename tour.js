b387855dee65c7dd6db52b03344e097ada959f14
Gampol Thitinilnithi <gampolt@gmail.com>
2012-12-25 11:29:53 +0700

    Color on Click : Session and template function

--- tour.js
+++ tour.js
@@ -4,4 +4,14 @@ if (Meteor.isClient) {
   Template.color_list.colors = function () {
     return Colors.find({}, {sort: {likes:-1, name:1}});
   };
+  
+  Template.color_info.maybe_selected = function(){
+    return Session.equals('session_color', this._id) ? "selected" : ""; 
+  };
+  
+  Template.color_info.events = {
+    'click': function(){
+      Session.set('session_color', this._id);
+    }
+  }
 }
